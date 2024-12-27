57246903afaa9a00ae668249ca2c4a3a732e831b
Filipe <filipeferreiramariz@outlook.com>
2024-12-27 15:25:49 -0300

    feat: added file upload

--- src/app/worker-enrollment/worker-enrollment.component.ts
+++ src/app/worker-enrollment/worker-enrollment.component.ts
@@ -18,6 +18,8 @@ import axios, { AxiosResponse } from 'axios';
   // exports: [WorkerEnrollmentComponent]
 })
 export class WorkerEnrollmentComponent implements OnInit {
+  public file!: File;
+  public fileContent!: ArrayBuffer;
   public data: IFormaData = formData;
   public teams: ITeamsGroup = teamsGroup;
   public clubs: any;
@@ -28,15 +30,58 @@ export class WorkerEnrollmentComponent implements OnInit {
     this.clubs = response.data;
   }
 
+  // public async sendData() {
+  //   try {
+  //     console.log('file', this.file)
+  //     // const response: AxiosResponse = await axios.post('http://localhost:3000/unit', this.data)
+
+  //     console.log('file', this.file)
+  //     const resp: AxiosResponse = await axios.post('http://localhost:3000/clubs/payment', {
+  //       id: this.data.id,
+  //       file: this.fileContent
+  //     })
+
+  //     // return response.data;
+  //   } catch (error) {
+  //     throw new Error('AxiosError')
+  //   }
+  // }
+
   public async sendData() {
     try {
-      console.log(this.data)
-      const response: AxiosResponse = await axios.post('http://localhost:3000/unit', this.data)
-      return response.data;
+      const formData = new FormData();
+      formData.append('file', this.file); // Adiciona o arquivo
+      formData.append('id', this.data.id); // Adiciona o ID ou outros dados
+
+      const response: AxiosResponse = await axios.post('http://localhost:3000/clubs/payment', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Certifica que o formato é multipart
+        },
+      });
+
+      console.log('Upload bem-sucedido:', response.data);
     } catch (error) {
-      throw new Error('AxiosError')
+      console.error('Erro no envio:', error);
     }
   }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      this.file = input.files[0];
+      console.log('Arquivo selecionado:', this.file);
+
+      // Usando FileReader para ler o conteúdo do arquivo
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.fileContent = reader.result as ArrayBuffer;
+        console.log('Conteúdo do arquivo lido:', this.fileContent);
+      };
+      reader.readAsArrayBuffer(this.file);
+    }
+  }
+
 
   // async getUnitsByClub(clubId: Event) {
   //   const response: AxiosResponse = await axios.get(`http://localhost:3000/unit/${clubId}`)
